f165afa2e2a4424ef803ab28ea736b35c0afee4c
Tamtikorn <gan_zaza_12@hotmail.com>
2023-10-06 17:25:48 +0700

    new

---
+++ front-end/vite-project/src/app.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'preact/hooks'
+import './app.css'
+import Header from './header'
+
+export function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
---
+++ front-end/vite-project/src/header.jsx
@@ -0,0 +1,12 @@
+import "./header.css"
+
+function Header() {
+    
+    return(
+        <div className="main">
+            
+        </div>
+    )
+}
+
+export default Header
