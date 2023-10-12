4389fa38e8623ab829d1a7e36401ce93fc6839fd
Tamtikorn <gan_zaza_12@hotmail.com>
2023-10-12 15:01:39 +0700

    new

--- front-end/src/profile/profile.jsx
+++ front-end/src/profile/profile.jsx
@@ -1,11 +1,26 @@
 import './profile.css'
 import Header from '../templent/header'
 import Footer from '../templent/footer'
-import { useState } from 'preact/hooks'
 import Card from '../card'
+import { useState, useRef } from 'preact/hooks'
 
 export function Profile() {
     const [isPost,setIsPost] = useState(true)
+    const elementRefs = Array.from({ length: 3 }, () => useRef(null));
+    const [mode, setMode] = useState('post')
+    const handleElementClick = (clickedIndex) => {
+        elementRefs.forEach((ref, index) => {
+          if (index === clickedIndex) {
+            ref.current.style.backgroundColor = 'orange';
+            ref.current.style.color = 'white';
+          } else {
+            ref.current.style.backgroundColor = 'white';
+            ref.current.style.border = '3px solid orange';
+            ref.current.style.color = 'black'
+          }
+        });
+      };
+      
     function toPost() {
         setIsPost(true)
     }
@@ -14,26 +29,18 @@ export function Profile() {
         setIsPost(false)
     }
     
-    const notIsStyle = {
-        backgroundColor: "white",
-        border: "3px solid orange",
-    }
-    const isStyle = {
-        backgroundColor: "orange",
-        color: "white"
-    }
-    
     return(
         <>
             <Header />
             <div className='bar'>
                 <p className='page-text'>Profile</p>
-                <button onClick={toPost} style={isPost ? isStyle : notIsStyle}>My Post</button> 
-                <button onClick={toVideo} style={!isPost ? isStyle : notIsStyle}>My Video</button> 
+                <button onClick={() => {handleElementClick(0);setMode('post')}}  ref={elementRefs[0]} key={0}>My Post</button> 
+                <button onClick={() => {handleElementClick(1);setMode('video')}} ref={elementRefs[1]} key={1}>My Video</button> 
+                <button onClick={() => {handleElementClick(2);setMode('user')}} ref={elementRefs[2]} key={2}>Edit Profile</button>
             </div>
             <div className='contents'>
                 {console.log(isPost)}
-                {isPost &&
+                {mode == 'post' &&
                 <>
                     <Card mode="pic" isPro={true}/>
                     <Card mode="pic" isPro={true}/>
@@ -41,9 +48,9 @@ export function Profile() {
                     <Card mode="pic" isPro={true}/>
                     <Card mode="pic" isPro={true}/>
                     <Card mode="pic" isPro={true}/>
-                </> 
+                </>
                 }
-                {!isPost &&
+                {mode == 'video' &&
                 <>
                     <Card mode="video" isPro={true}/>
                     <Card mode="video" isPro={true}/>
@@ -52,6 +59,11 @@ export function Profile() {
                     <Card mode="video" isPro={true}/>
                     <Card mode="video" isPro={true}/>
                 </>
+                }
+                {mode == 'user' &&
+                <div className='user-edit'>
+                    
+                </div>
                 }
             </div>
             <Footer />
